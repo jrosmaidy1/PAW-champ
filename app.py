fc5df8f0cf593123475f57b0f5f17acee2bac145
kayyon <89946346+kayyon@users.noreply.github.com>
2022-03-20 04:52:45 -0400

    added form functionality

--- app.py
+++ app.py
@@ -2,18 +2,26 @@ import os
 import requests
 from twilio.rest import Client
 import flask
+
+from flask import request
+
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
 app = flask.Flask(__name__)
 
+# twilio credential
 account_sid = os.getenv("SID")
 auth_token = os.getenv("TOKEN")
 client = Client(account_sid, auth_token)
 
+# catAPI
 catUrl = "https://api.thecatapi.com/v1/images/search?format=json"
+# catFactAPI
 factUrl = "https://catfact.ninja/fact?max_length=140"
 
+# catAPI credential
 catData = {}
 catHeaders = {"Content-Type": "application/json", "x-api-key": os.getenv("KEY")}
 factData = {}
@@ -26,18 +34,24 @@ factRes = requests.get(factUrl, headers=factHeaders, data=factData)
 catR = catRes.json()
 factR = factRes.json()
 
+
+# catAPI for image
 images = catR[0]["url"]
+# catFactAPI for fact
 fact = factR["fact"]
 
-message = client.messages.create(
-    from_="+13185943649",
-    messaging_service_sid=os.getenv("MID"),
-    body="\n***CAT fact of the day!" + "\U0001F638" + "***\n" + fact,
-    media_url=images,
-    to="+14703549666",
-)
+# use twilio to send messages
+def create_message(phoneNumber):
+    message = client.messages.create(
+        from_="+13185943649",
+        messaging_service_sid=os.getenv("MID"),
+        body="\n***CAT fact of the day!" + "\U0001F638" + "***\n" + fact,
+        media_url=images,
+        to="+1" + phoneNumber,
+    )
 
-print(message.sid)
+    print(message.sid)
+
 
 @app.route("/")
 def main():
@@ -54,11 +68,12 @@ def about():
     return flask.render_template("about.html")
 
 
-@app.route("/ourService")
+@app.route("/ourService", methods=["GET", "POST"])
 def ourService():
+    if request.method == "POST":
+        phoneNumber = request.form.get("phoneNumber")
+        create_message(phoneNumber)
     return flask.render_template("ourService.html")
 
 
-app.run(
-    debug=True,
-)
+app.run(host="127.0.0.1", port=5000, debug=True)
